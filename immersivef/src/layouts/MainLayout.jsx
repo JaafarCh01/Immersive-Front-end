2b94cb3e94d956315659c3fa8e04242936934ce3
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-07-29 13:06:20 +0100

    Added Cards (need to modify css)

--- immersivef/src/layouts/MainLayout.jsx
+++ immersivef/src/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import ChipTabs from "../components/ChipTabs";
 
+
 const MainLayout = () => {
   return (
     <div className="flex flex-col h-screen">
--- immersivef/src/pages/HomePage.jsx
+++ immersivef/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero';
-
+import Cards from '../components/Cards';
 
 export const HomePage = () => {
   return ( <>
         <Hero />
+        <Cards />
          </>
   )
 }
