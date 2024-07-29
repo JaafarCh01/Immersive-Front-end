0de1b44a110c86d6b4ce133e5271899e1aba4adf
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-07-29 13:33:47 +0100

    Footer

--- immersivef/src/pages/HomePage.jsx
+++ immersivef/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 
 export const HomePage = () => {
   return ( <>
         <Hero />
         <Cards />
+        <Footer />
          </>
   )
 }
