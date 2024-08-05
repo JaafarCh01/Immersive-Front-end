ce57b0d572a7e8bf34ffcb97b217f88dd58c015b
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-08-05 11:26:05 +0100

    Hero Fix

--- immersivef/src/components/Hero.jsx
+++ immersivef/src/components/Hero.jsx
@@ -36,7 +36,7 @@ export const Hero = () => {
     >
       <div className="relative z-10 flex flex-col items-center">
         <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
-          Beta Now Live!
+          Welcome to the web3
         </span>
         <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
             Immerse yourself in the future of the web
@@ -58,7 +58,7 @@ export const Hero = () => {
           }}
           className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
         >
-          Start free trial
+          Start 
           <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
         </motion.button>
       </div>
